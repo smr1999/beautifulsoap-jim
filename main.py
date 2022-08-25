7420380c405319923aa71424e1ad31efa390f95d
smr <mohammad>
2022-08-25 23:45:24 +0430

    Part 4

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
+import requests
 from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    with open('index.html',mode='r') as html_file:
-        content = html_file.read()
-        
-        soup = BeautifulSoup(content,'lxml')
-        
-        course_cards = soup.find_all('div',class_='card')
 
+    keyword = 'python'
 
-        for course in course_cards:
-            # print(course.prettify())
-            # print(course.h5)
-            course_name = course.h5.text
-            course_price = course.a.text.split()[-1]
-            print(course_name,':',course_price)
+    html_text = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={keyword}&txtLocation=').text
+
+    soup = BeautifulSoup(html_text,'lxml')
+
+    total_posts = int(soup.find('span',id='totolResultCountsId').text.strip())
+
+    per_page = 25
+    
+    # total_pages = total_posts//per_page+1
+    total_pages =5
+
+    jobs = []
+
+    for i in range(1,total_pages+1):
+        html_text = requests.get(f'https://www.timesjobs.com/candidate/job-search.html?from=submit&actualTxtKeywords={keyword}&searchBy=0&rdoOperator=OR&searchType=personalizedSearch&luceneResultSize={str(per_page)}&postWeek=60&txtKeywords=python&pDate=I&sequence={str(i)}&startPage={str((i%10)*10+1)}').text
         
+        soup = BeautifulSoup(html_text,'lxml')
+        jobs_ = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
 
+        for job in jobs_:
+            published_date = job.find('span',class_='sim-posted').span.text.strip()
+            if 'Posted few days ago' in published_date:
+                company_name = job.find('h3',class_='joblist-comp-name').text.strip()
+
+                skills = job.find('span',class_='srp-skills').text.split(' , ')
+                skills = [skill.strip() for skill in skills]
+
+                published_date = job.find('span',class_='sim-posted').span.text.strip()
+
+                jobs.append((company_name,skills))
+        print(f'page {i}')
+    
+    print(jobs)
